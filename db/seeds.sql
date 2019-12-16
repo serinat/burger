fd15b3b16cd9a00931f8405dca0890413bc78f78
Serina Tsang <serinat@uw.edu>
2019-12-16 12:26:33 -0800

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Veggie");
+INSERT INTO burgers (burger_name) VALUES ("Plain");
+INSERT INTO burgers (burger_name) VALUES ("Chicken");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royal", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("California", true);
